b3f1ae94137ae03169657baa88baba93e59785a2
Herbert Lange <sonofchaos@gmx.de>
2012-03-01 17:56:01 +0100

    Program to convert images to grayscale and merge them

---
+++ merge.pl
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+use Image::Imlib2;
+
+{
+  my $i1 = Image::Imlib2->load("bild1.png");
+  my $i2 = Image::Imlib2->load("bild1.png");
+  my $image = Image::Imlib2->new(200, 200);
+}
